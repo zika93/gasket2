1db9bc0943e569d14d5dbaff23fe633f0365d1be
Nemanja <neca@svrljig.net>
2018-11-07 02:23:05 +0100

    FIx

--- assets/js/language.js
+++ assets/js/language.js
@@ -52,23 +52,23 @@ var language =
     }
 }
 
-function setCookie(name,value,days) {
+function setCookie(name, value, days) {
     var expires = "";
     if (days) {
         var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
@@ -79,6 +79,12 @@ function laguageChange() {
         setCookie('language', 'rs', 1000);
         currentLanguage = 'rs';
         document.getElementById('lang_rs').style.display = 'none';
+    }
+    else {
+        if (currentLanguage == 'en')
+            document.getElementById('lang_rs').style.display = 'none';
+        else
+            document.getElementById('lang_en').style.display = 'none';
     }
     let obj = language[currentLanguage];
     for (let propertyName in obj) {
@@ -94,7 +100,7 @@ function setEN() {
 }
 
 function setRS() {
-    setCookie('language', 'en', 1000);
+    setCookie('language', 'rs', 1000);
     laguageChange();
     document.getElementById('lang_rs').style.display = 'none';
     document.getElementById('lang_en').style.display = 'block';
